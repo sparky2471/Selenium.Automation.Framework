a429a2ae126c37a833b8fb97f1c9b4fe23f1100c
Stephon Parker <sgparker62@gmail.com>
2020-11-13 18:14:00 -0500

    adding method for interacting with ng web elements

--- Selenium.Automation.Framework.Base/Extensions/NGElementExtensions.cs
+++ Selenium.Automation.Framework.Base/Extensions/NGElementExtensions.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using Protractor;
+using System.Collections.ObjectModel;
+
 namespace Selenium.Automation.Framework.Extensions
 {
     public static class NGElementExtensions
     {
-
+        public static void TrySelectOption(this NgWebElement selectionPanel, int index)
+        {
+            ReadOnlyCollection<NgWebElement> options = selectionPanel.FindElements(By.TagName("mat-option"));
+            options[index].Click();
+        }
     }
 }
